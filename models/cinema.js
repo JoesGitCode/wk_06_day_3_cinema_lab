92321a440d409223a62c86a67128a5fdbef4dc5c
Joseph Young <jsyoung0891@gmail.com>
2019-05-08 14:15:01 +0100

    second test passed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,14 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.allFilmTitles = function(){
+  return this.films.map(film => film.title)
+}
+
+Cinema.prototype.findFilmByTitle = function(findFilm){
+  const wholeFilm =  this.films.find(film => findFilm.title === film.title)
+  return wholeFilm.title
+}
+
+
 module.exports = Cinema;
